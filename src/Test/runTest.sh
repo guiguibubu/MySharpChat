231375c3e7557f7958ed665ce816b3d82da10f1b
guiguibubu <guibu44@hotmail.fr>
2022-07-02 04:15:54 +0200

    Fix linux support for post build task in Test

---
+++ src/Test/runTest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NUNIT_CONSOLE_PATH=$1
+ASSEMBLY_PATH=$2
+WORK_DIRECTORY=$3
+
+function failParameters {
+	echo 3 parameters are needed
+	echo 1 : full path to nunit console
+	echo 2 : full path to assembly to test
+	echo 3 : full path to output directory for nunit files
+	exit 1
+}
+
+function isEnvVarDefined {
+	envar=$1
+	if [[ -z "${envar}" ]]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+echo $(isEnvVarDefined NUNIT_CONSOLE_PATH)
+
+if isEnvVarDefined NUNIT_CONSOLE_PATH; then failParameters; fi
+if isEnvVarDefined ASSEMBLY_PATH; then failParameters; fi
+if isEnvVarDefined WORK_DIRECTORY; then failParameters; fi
+
+if [ ! "$MYSHARPCHAT_SKIP_TEST" = "true" ]; then
+	$NUNIT_CONSOLE_PATH $ASSEMBLY_PATH --work $WORK_DIRECTORY
+else
+	echo MYSHARPCHAT_SKIP_TEST set to "$MYSHARPCHAT_SKIP_TEST". Test will be skip	
+fi
