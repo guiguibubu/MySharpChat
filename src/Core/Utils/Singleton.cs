1ecbc8440a5ee0dd7383fe433c62e68a609df35e
guiguibubu <guibu44@hotmail.fr>
2022-07-26 17:16:32 +0200

    Make singleton Instance not nullab

--- src/Core/Utils/Singleton.cs
+++ src/Core/Utils/Singleton.cs
@@ -7,14 +7,14 @@ namespace MySharpChat.Core.Utils
 {
     public abstract class Singleton<T> where T : class
     {
-        private static Lazy<T?> _instance = new Lazy<T?>(() => GetInstance());
-        private static Type _myType = typeof(T);
+        private readonly static Lazy<T?> s_instance = new Lazy<T?>(() => GetInstance());
+        private readonly static Type s_myType = typeof(T);
         public const string INSTANCE_CREATOR_NAME = "GetInstanceImp";
 
         private static T? GetInstance()
         {
             T? instance = null;
-            Type type = _myType;
+            Type type = s_myType;
 #pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
             IEnumerable<MethodInfo> possibleMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
                             .Where(m => m.Name == Singleton<object>.INSTANCE_CREATOR_NAME && m.GetParameters().Length == 0 && m.ReturnType == type);
@@ -28,7 +28,7 @@ namespace MySharpChat.Core.Utils
                 if (possibleConstructor != null)
                     instance = (T)possibleConstructor.Invoke(null);
             }
-            if (_instance == null)
+            if (s_instance == null)
             {
                 if (nbPossibleMethods == 1)
                 {
@@ -42,6 +42,6 @@ namespace MySharpChat.Core.Utils
             return instance;
         }
 
-        public static T? Instance { get { return _instance.Value; } }
+        public static T Instance { get { return s_instance.Value ?? throw new InvalidOperationException($"Instanciation of {s_myType} failed. Instance can't be null"); } }
     }
 }
