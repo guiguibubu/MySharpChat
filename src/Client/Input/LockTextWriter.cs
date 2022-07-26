df919edf6d4bd19464fcfc11867264d68739c3c0
guiguibubu <guibu44@hotmail.fr>
2022-07-26 15:17:11 +0200

    [WIP] First implementation to support response printing and moving input section

---
+++ src/Client/Input/LockTextWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharpChat.Client.Input
+{
+    internal abstract class LockTextWriter : TextWriter
+    {
+        public abstract IDisposable Lock();
+        public abstract bool IsLocked { get; }
+    }
+}
