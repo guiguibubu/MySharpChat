a0b857d8ed6d3129d3e728b9591dbdc9cd6a3706
guiguibubu <guibu44@hotmail.fr>
2022-07-26 17:19:36 +0200

    Move Lockable logic to Core

--- src/Client.Console.UI/ConsoleOutputWriter.cs
+++ src/Client.Console.UI/ConsoleOutputWriter.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using MySharpChat.Core.Utils;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MySharpChat.Client.Input
 {
---
+++ src/Core/Utils/ILockable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharpChat.Core.Utils
+{
+    public interface ILockable
+    {
+        IDisposable Lock();
+        bool IsLocked { get; }
+    }
+}
