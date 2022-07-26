b2a16cada30b9534b6cd15211417050872aff9ac
guiguibubu <guibu44@hotmail.fr>
2022-07-26 17:19:36 +0200

    Move Lockable logic to Core

--- src/Core/Utils/LockTextWriter.cs
+++ src/Core/Utils/LockTextWriter.cs
@@ -5,11 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MySharpChat.Client.Input
+namespace MySharpChat.Core.Utils
 {
-    public abstract class LockTextWriter : TextWriter
+    public abstract class LockTextWriter : TextWriter, ILockable
     {
-        public abstract IDisposable Lock();
         public abstract bool IsLocked { get; }
+
+        public abstract IDisposable Lock();
     }
 }
