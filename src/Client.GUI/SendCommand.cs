7b3a9730c12f619b61c8756759b0c1524aa6c2a3
guiguibubu <guibu44@hotmail.fr>
2022-07-28 20:40:45 +0200

    [WIP] First implementation of a Client in WPF. No actual logic.

---
+++ src/Client.GUI/SendCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySharpChat.Client.GUI
+{
+    internal class SendCommand : ICommand
+    {
+        private bool _isExecuting = false;
+
+        public bool CanExecute(object parameter)
+        {
+            return !(_isExecuting);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public async void Execute(object parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+
+            if (parameter is ChatUserControl chatUC)
+            {
+                _isExecuting = true;
+                OnCanExecuteChanged();
+                try
+                {
+                    chatUC.Send();
+                }
+                finally
+                {
+                    _isExecuting = false;
+                    OnCanExecuteChanged();
+                }
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("{0} must be of type {1}", nameof(parameter), typeof(ChatUserControl)));
+            }
+        }
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
