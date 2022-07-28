82b99f36fe222f1aada2179cbcfe97998db749c6
guiguibubu <guibu44@hotmail.fr>
2022-07-28 20:40:45 +0200

    [WIP] First implementation of a Client in WPF. No actual logic.

---
+++ src/Client.GUI/ChatUserControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySharpChat.Client.GUI
+{
+    /// <summary>
+    /// Interaction logic for ChatUserControl.xaml
+    /// </summary>
+    public partial class ChatUserControl : UserControl
+    {
+        public ChatUserControl()
+        {
+            InitializeComponent();
+
+            OutputBox.TextWrapping = TextWrapping.Wrap;
+            OutputBox.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            OutputBox.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+            InputBox.KeyDown += InputBox_KeyDown; 
+
+            SendButton.Command = new SendCommand();
+            SendButton.CommandParameter = this;
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Key key = e.Key;
+            if (key == Key.Enter)
+                Send();
+        }
+
+        public void Send()
+        {
+            string text = InputBox.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                OutputBox.AppendText(text + Environment.NewLine);
+                OutputBoxScroller.ScrollToEnd();
+                InputBox.Text = "";
+                InputBox.Focus();
+            }
+        }
+    }
+}
---
+++ src/Client.GUI/ConnectCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MySharpChat.Client.GUI
+{
+    internal class ConnectCommand : ICommand
+    {
+        private bool _isExecuting = false;
+
+        public bool CanExecute(object parameter)
+        {
+            return !(_isExecuting);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public async void Execute(object parameter)
+        {
+            if(parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+
+            if(parameter is ConnectionUserControl connectionUC)
+            {
+                _isExecuting = true;
+                OnCanExecuteChanged();
+                try
+                {
+                    connectionUC.OnConnectionSuccess();
+                }
+                finally
+                {
+                    _isExecuting = false;
+                    OnCanExecuteChanged();
+                }
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("{0} must be of type {1}", nameof(parameter), typeof(ConnectionUserControl)));
+            }
+        }
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
