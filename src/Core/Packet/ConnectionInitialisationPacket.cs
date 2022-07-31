e3645f9145be4df4e41618f16bf854ac945a6863
guiguibubu <guibu44@hotmail.fr>
2022-07-31 20:37:34 +0200

    Use a guid to identify packets. Broadcast when user join or leave

---
+++ src/Core/Packet/ConnectionInitialisationPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharpChat.Core.Packet
+{
+    [Serializable]
+    public class ConnectionInitialisationPacket
+    {
+        public ConnectionInitialisationPacket(Guid sessionId)
+        {
+            SessionId = sessionId;
+        }
+
+        public Guid SessionId { get; set; }
+    }
+}
