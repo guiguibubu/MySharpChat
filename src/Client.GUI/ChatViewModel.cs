57023b9180aea0f8ba999300036726fab47fa67f
guiguibubu <guibu44@hotmail.fr>
2022-08-02 02:57:51 +0200

    Improve Chat sending command managment and use one UI element per message

--- src/Client.GUI/ChatViewModel.cs
+++ src/Client.GUI/ChatViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,24 @@ namespace MySharpChat.Client.GUI
     internal class ChatViewModel
     {
         public GuiClientImpl Client { get; private set; }
+        public ObservableCollection<string> Messages { get; private set; } = new ObservableCollection<string>();
+        public string InputMessage { get; set; } = "";
 
         public ChatViewModel(GuiClientImpl client)
         {
             Client = client;
+        }
+
+        public event Action OnSendSuccessEvent = () => { };
+
+        public void OnSendSuccess()
+        {
+            string text = InputMessage;
+            if (!string.IsNullOrEmpty(text))
+            {
+                Messages.Add(text);
+                OnSendSuccessEvent();
+            }
         }
     }
 }
