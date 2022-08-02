e26f17b405a112c3bef6ac9e17bd552dfbd962f4
guiguibubu <guibu44@hotmail.fr>
2022-08-02 15:44:29 +0200

    Add support for chat history, and share it to new conneted users

--- src/Server/ChatRoom.cs
+++ src/Server/ChatRoom.cs
@@ -11,6 +11,7 @@ namespace MySharpChat.Server
     public class ChatRoom
     {
         private readonly List<ChatSession> m_connectedSessions = new List<ChatSession>();
+        private readonly List<ChatMessage> m_messages = new List<ChatMessage>();
         private readonly Guid _serverId;
 
         public ChatRoom(Guid serverId)
@@ -40,10 +41,17 @@ namespace MySharpChat.Server
         {
             UpdateUsernameIfNecessary(session);
 
-            UserStatusPacket package = new UserStatusPacket(session.ClientUsername, true);
+            UserStatusPacket package = new UserStatusPacket(session.User.Username, true);
             PacketWrapper packet = new PacketWrapper(_serverId, package);
 
             Broadcast(session, packet);
+
+            foreach (ChatMessage message in m_messages)
+            {
+                ChatPacket chatPackage = new ChatPacket(message.Message);
+                PacketWrapper messagePacket = new PacketWrapper(_serverId, chatPackage);
+                session.NetworkModule.Send(messagePacket);
+            }
 
             m_connectedSessions.Add(session);
 
@@ -53,7 +61,7 @@ namespace MySharpChat.Server
         {
             m_connectedSessions.Remove(session);
 
-            UserStatusPacket package = new UserStatusPacket(session.ClientUsername, false);
+            UserStatusPacket package = new UserStatusPacket(session.User.Username, false);
             PacketWrapper packet = new PacketWrapper(_serverId, package);
 
             Broadcast(session, packet);
@@ -61,13 +69,13 @@ namespace MySharpChat.Server
 
         private void OnUsernameChange(ChatSession session, string oldUsername)
         {
-            bool usernameChanged = !string.Equals(session.ClientUsername, oldUsername, StringComparison.InvariantCulture);
+            bool usernameChanged = !string.Equals(session.User.Username, oldUsername, StringComparison.InvariantCulture);
             if (!usernameChanged)
                 return;
 
             UpdateUsernameIfNecessary(session);
 
-            string message = $"User name changed for \"{oldUsername}\" to \"{session.ClientUsername}\"";
+            string message = $"User name changed for \"{oldUsername}\" to \"{session.User.Username}\"";
             ChatPacket package = new ChatPacket(message);
             PacketWrapper packet = new PacketWrapper(_serverId, package);
 
@@ -76,6 +84,9 @@ namespace MySharpChat.Server
 
         private void Broadcast(ChatSession origin, PacketWrapper packet)
         {
+            if (packet.Package is ChatPacket chatPacket)
+                m_messages.Add(new ChatMessage(origin.User, chatPacket.Message));
+
             IEnumerable<ChatSession> sessionToBroadcast = m_connectedSessions;
             foreach (ChatSession session in sessionToBroadcast)
             {
@@ -85,7 +96,7 @@ namespace MySharpChat.Server
 
         private bool IsUserNameAvailable(ChatSession session, string username)
         {
-            return !m_connectedSessions.Where(s => s != session).Select(s => s.ClientUsername).Contains(username);
+            return !m_connectedSessions.Where(s => s != session).Select(s => s.User.Username).Contains(username);
         }
 
         private string GenerateNewUsername(ChatSession session, string currentUsername)
@@ -102,14 +113,14 @@ namespace MySharpChat.Server
 
         private void UpdateUsernameIfNecessary(ChatSession session)
         {
-            string currentUsername = session.ClientUsername;
+            string currentUsername = session.User.Username;
 
             if (!IsUserNameAvailable(session, currentUsername))
             {
-                session.ClientUsername = GenerateNewUsername(session, currentUsername);
+                session.User.Username = GenerateNewUsername(session, currentUsername);
             }
 
-            ClientInitialisationPacket connectInitPacket = new ClientInitialisationPacket(session.ClientId, session.ClientUsername);
+            ClientInitialisationPacket connectInitPacket = new ClientInitialisationPacket(session.ClientId, session.User.Username);
             session.NetworkModule.Send(new PacketWrapper(_serverId, connectInitPacket));
         }
     }
