c1ea01a2f4da68e34e260c56c7aae0da53edd19c
guiguibubu <guibu44@hotmail.fr>
2022-08-25 03:53:22 +0200

    Add MAUI support for CLient

---
+++ src/Client.GUI.MAUI/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using System;
+using System.Windows;
+
+namespace MySharpChat.Client.GUI.MAUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ContentPage
+    {
+        private ContentView currentUC;
+        private readonly ChatUserControl chatUC;
+        private readonly ConnectionUserControl connectionUC;
+        private readonly MainWindowViewModel m_viewModel;
+
+        internal MainWindow(MainWindowViewModel viewModel)
+        {
+            InitializeComponent();
+
+            m_viewModel = viewModel;
+
+            ConnectionViewModel connectionViewModel = new ConnectionViewModel(m_viewModel.Client);
+            connectionViewModel.OnConnectionSuccessEvent += OnConnectionSucess;
+
+            connectionUC = new ConnectionUserControl(connectionViewModel);
+
+            ChatViewModel chatViewModel = new ChatViewModel(m_viewModel.Client);
+            chatUC = new ChatUserControl(chatViewModel);
+            chatUC.OnDisconnectionEvent += OnDisconnection;
+            chatUC.OnDisconnectionEvent += connectionViewModel.OnDisconnection;
+            currentUC = connectionUC;
+
+            WindowGrid.Children.Add(currentUC);
+        }
+
+        private void OnDisconnection(bool manual)
+        {
+            if (MainThread.IsMainThread)
+            {
+                WindowGrid.Children.Remove(currentUC);
+                currentUC = connectionUC;
+                WindowGrid.Children.Add(currentUC);
+            }
+            else
+            {
+                MainThread.InvokeOnMainThreadAsync(() => OnDisconnection(manual));
+            }
+        }
+
+        private void OnConnectionSucess()
+        {
+            if (MainThread.IsMainThread)
+            {
+                WindowGrid.Children.Remove(currentUC);
+                currentUC = chatUC;
+                WindowGrid.Children.Add(currentUC);
+            }
+            else
+            {
+                MainThread.InvokeOnMainThreadAsync(() => OnConnectionSucess());
+            }
+        }
+    }
+}
